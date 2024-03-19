7d3e44f9c89e6853317f3448b1c2eeb8898a082d
orzarhi <or.zarhi11@gmail.com>
2024-03-19 16:07:31 +0200

    Add VSCode settings and database configuration

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,6 @@
+import { db } from '@/db'
+import { productsTable } from '@/db/schema'
+import { sql } from 'drizzle-orm'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
@@ -7,12 +10,16 @@ interface PageProps {
     }
 }
 
-export default function page({ searchParams }: PageProps) {
+export default async function page({ searchParams }: PageProps) {
     const query = searchParams.query
 
     if (Array.isArray(query) || !query) {
         return redirect('/')
     }
+
+    // let products = await db.select().from(productsTable).where(
+    //     sql`to_tsvector(name) @@ plainto_tsquery(${query})`
+    // )
 
     return (
         <div>page</div>
